db5f8c1b86334d6ba4096710c7f66676caa0d595
Kadir Korkmaz <kadir33korkmaz@gmail.com>
2022-09-15 15:22:03 +0300

    done content area

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,25 +1,42 @@
 import React from 'react';
+import noImage from '../img/no-post-image.png';
 import { BiBlock } from 'react-icons/bi';
 import { RiDeleteBin6Line } from 'react-icons/ri';
 import { CgMoreO } from 'react-icons/cg';
-import noImage from '../img/no-post-image.png';
+import { MdOutlineModeComment, MdDone } from 'react-icons/md';
+import { FaFacebookF, FaRetweet } from 'react-icons/fa';
+import {
+  AiOutlineLike,
+  AiOutlineShareAlt,
+  AiOutlineEye,
+  AiOutlineTwitter,
+  AiOutlineInstagram,
+  AiOutlineHeart,
+} from 'react-icons/ai';
 
-import { AiOutlineLike, AiOutlineShareAlt, AiOutlineEye } from 'react-icons/ai';
-
-import { MdOutlineModeComment } from 'react-icons/md';
-
-import { FaFacebookF } from 'react-icons/fa';
-function Card({ date, content, image }) {
+function Card({ date, content, image, channel, status }) {
   return (
     <div className='card'>
-      <div className='cardLeftBar'>
-        <FaFacebookF size={23} />
+      <div className={`cardLeftBar status${status}`}>
+        {channel === 'facebook' ? (
+          <FaFacebookF size={23} />
+        ) : channel === 'twitter' ? (
+          <AiOutlineTwitter size={23} />
+        ) : (
+          <AiOutlineInstagram size={23} />
+        )}
       </div>
       <div className='cardRightArea'>
         <div className='cardTopActions'>
           <div className='cardDate'>{date}</div>
           <div className='cardActions'>
-            <BiBlock size={18} />
+            {status === 0 ? (
+              <MdDone size={18} />
+            ) : status === 1 ? (
+              <BiBlock size={18} />
+            ) : (
+              ''
+            )}
             <RiDeleteBin6Line size={18} />
             <CgMoreO size={18} />
           </div>
@@ -27,26 +44,34 @@ function Card({ date, content, image }) {
         <div className='cardContent'>{content}</div>
         <img
           src={image}
-          alt='test'
+          alt='post img'
           onError={(e) => {
             e.currentTarget.src = noImage;
           }}
         ></img>
         <div className='cardBottomActions'>
           <div>
-            <AiOutlineLike size={17} />
+            {channel === 'twitter' ? (
+              <AiOutlineHeart size={20} />
+            ) : (
+              <AiOutlineLike size={20} />
+            )}
             <span>0</span>
           </div>
           <div>
-            <MdOutlineModeComment size={17} />
+            {channel === 'twitter' ? (
+              <FaRetweet size={20} />
+            ) : (
+              <MdOutlineModeComment size={20} />
+            )}
             <span>0</span>
           </div>
           <div>
-            <AiOutlineShareAlt size={17} />
+            <AiOutlineShareAlt size={20} />
             <span>0</span>
           </div>
           <div>
-            <AiOutlineEye size={17} />
+            <AiOutlineEye size={20} />
             <span>0</span>
           </div>
         </div>
--- src/components/Main.js
+++ src/components/Main.js
@@ -4,7 +4,16 @@ import data from '../data.json';
 import pp from '../img/pp.svg';
 
 function Main() {
-  console.log(Object.values(data.posts_by_date));
+  const handleDate = (date) => {
+    let mmddyyyy = new Date(date)
+      .toLocaleDateString('en-US', {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+      })
+      .split(' ');
+    return `${mmddyyyy[1].replace(',', '')} ${mmddyyyy[0]} ${mmddyyyy[2]}`;
+  };
 
   return (
     <div className='main'>
@@ -21,20 +30,26 @@ function Main() {
         </div>
       </div>
 
-      {Object.values(data.posts_by_date).map((post, index) => (
-        <div className='divByDate'>
-          <div className='date'>{Object.keys(data.posts_by_date)[index]}</div>
-          <div className='cardsArea'>
-            {post.map((subpost) => (
-              <Card
-                content={subpost.entry.message}
-                date={subpost.published_at}
-                image={subpost.entry.image}
-              />
-            ))}
+      {Object.values(data.posts_by_date)
+        .sort()
+        .map((post, index) => (
+          <div className='divByDate'>
+            <div className='date'>
+              {handleDate(Object.keys(data.posts_by_date)[index])}
+            </div>
+            <div className='cardsArea'>
+              {post.map((subpost) => (
+                <Card
+                  content={subpost.entry.message}
+                  date={subpost.published_at}
+                  image={subpost.entry.image}
+                  status={subpost.status}
+                  channel={subpost.account.channel}
+                />
+              ))}
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
     </div>
   );
 }
